62281d19d024cbae52b5f805f0f4a4f2f247c969
pascal-codecat <pascal.zwicker@windowslive.com>
2017-06-27 01:28:38 +0200

    huge update

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,42 @@
+
+
 jQuery(document).ready(function( $ ) {
+
+  var counter = 0, // to keep track of current slide
+      $items = $('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
+      numItems = $items.length; // total number of slides
+
+  // this function is what cycles the slides, showing the next or previous slide and hiding all the others
+  var showCurrent = function(){
+      var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show
+
+    $items.removeClass('show'); // remove .show from whichever element currently has it
+    $items.eq(itemToShow).addClass('show');
+  };
+
+  // add click events to prev & next buttons
+  $('.next').on('click', function(){
+      counter++;
+      showCurrent();
+  });
+  $('.prev').on('click', function(){
+      counter--;
+      showCurrent();
+  });
+
+  // if touch events are supported then add swipe interactions using TouchSwipe https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+  if('ontouchstart' in window){
+    $('.diy-slideshow').swipe({
+      swipeLeft:function() {
+        counter++;
+        showCurrent();
+      },
+      swipeRight:function() {
+        counter--;
+        showCurrent();
+      }
+    });
+  }
 
   // Preloader
   $(window).on('load', function() {
@@ -73,7 +111,7 @@ jQuery(document).ready(function( $ ) {
   $("#header").sticky({topSpacing:0, zIndex: '50'});
 
   // Smoth scroll on page hash links
-  $('a[href*="#"]:not([href="#"])').on('click', function() {
+  $('a[href*="#"]:not([href="#"]):not([href="#carousel-example-generic"])').on('click', function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           if (target.length) {
