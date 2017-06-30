26489f96fc15efb325ffcdbae811a24cd3fa8c8a
pascal-codecat <pascal.zwicker@windowslive.com>
2017-06-30 23:50:01 +0200

    merge test

--- js/custom.js
+++ js/custom.js
@@ -110,37 +110,37 @@ jQuery(document).ready(function( $ ) {
   // Stick the header at top on scroll
   $("#header").sticky({topSpacing:0, zIndex: '50'});
 
-  // Smoth scroll on page hash links
-  $('a[href*="#"]:not([href="#"]):not([href="#carousel-example-generic"])').on('click', function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          if (target.length) {
-
-              var top_space = 0;
-
-              if( $('#header').length ) {
-                top_space = $('#header').outerHeight();
-              }
-
-              $('html, body').animate({
-                  scrollTop: target.offset().top - top_space
-              }, 1500, 'easeInOutExpo');
-
-              if ( $(this).parents('.nav-menu').length ) {
-                $('.nav-menu .menu-active').removeClass('menu-active');
-                $(this).closest('li').addClass('menu-active');
-              }
-
-              if ( $('body').hasClass('mobile-nav-active') ) {
-                  $('body').removeClass('mobile-nav-active');
-                  $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-                  $('#mobile-body-overly').fadeOut();
-              }
-
-              return false;
-          }
-      }
-  });
+  // Smooth scroll on page hash links
+  // $('a[href*="#"]:not([href="#"]):not([href="#carousel-example-generic"])').on('click', function() {
+  //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //         var target = $(this.hash);
+  //         if (target.length) {
+  //
+  //             var top_space = 0;
+  //
+  //             if( $('#header').length ) {
+  //               top_space = $('#header').outerHeight();
+  //             }
+  //
+  //             $('html, body').animate({
+  //                 scrollTop: target.offset().top - top_space
+  //             }, 1500, 'easeInOutExpo');
+  //
+  //             if ( $(this).parents('.nav-menu').length ) {
+  //               $('.nav-menu .menu-active').removeClass('menu-active');
+  //               $(this).closest('li').addClass('menu-active');
+  //             }
+  //
+  //             if ( $('body').hasClass('mobile-nav-active') ) {
+  //                 $('body').removeClass('mobile-nav-active');
+  //                 $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+  //                 $('#mobile-body-overly').fadeOut();
+  //             }
+  //
+  //             return false;
+  //         }
+  //     }
+  // });
 
   // Back to top button
   $(window).scroll(function() {
